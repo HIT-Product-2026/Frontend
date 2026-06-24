5e87b0379b0b69deb7d73ba8b5de34a224831ef8
Toan040606 <toannickson2k60604@gmail.com>
2026-06-24 18:11:34 +0700

    feat: add navigation

--- app/src/main/java/com/pando/app/features/auth/ui/login/LoginFragment.kt
+++ app/src/main/java/com/pando/app/features/auth/ui/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.pando.app.features.auth.ui.login
 
+import androidx.navigation.fragment.findNavController
+import com.pando.app.R
 import com.pando.app.core.base.BaseBottomSheet
 import com.pando.app.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,5 +12,13 @@ class LoginFragment : BaseBottomSheet<FragmentLoginBinding>(FragmentLoginBinding
     }
 
     override fun initActionView() {
+        binding.registerButton.setOnClickListener {
+            findNavController().popBackStack()
+            findNavController().navigate(R.id.action_startFragment_to_registerFragment)
+        }
+
+        binding.forgotPasswordBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginBottomSheet_to_forgotPasswordFragment)
+        }
     }
 }
--- app/src/main/java/com/pando/app/features/auth/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/pando/app/features/auth/ui/register/RegisterFragment.kt
@@ -1,5 +1,8 @@
 package com.pando.app.features.auth.ui.register
 
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import com.pando.app.R
 import com.pando.app.core.base.BaseBottomSheet
 import com.pando.app.databinding.FragmentRegisterBinding
 
@@ -8,5 +11,16 @@ class RegisterFragment : BaseBottomSheet<FragmentRegisterBinding>(FragmentRegist
     }
 
     override fun initActionView() {
+        binding.loginButton.setOnClickListener {
+            findNavController().popBackStack()
+            findNavController().navigate(R.id.action_startFragment_to_loginFragment)
+        }
+
+        binding.registerButton.setOnClickListener {
+            val bundle = Bundle().apply {
+                putString("is_register", "true")
+            }
+            findNavController().navigate(R.id.action_registerBottomSheet_to_verifyOtpFragment, bundle)
+        }
     }
 }
