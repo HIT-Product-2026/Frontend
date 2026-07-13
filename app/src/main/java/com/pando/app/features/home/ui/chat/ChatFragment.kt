7cbfccffe03a1ae2b60b6e382b8a14b2f02512c4
Toan040606 <toannickson2k60604@gmail.com>
2026-07-14 02:23:43 +0700

    feat: implement chat menu and chat fragments, add message layouts, and configure navigation

---
+++ app/src/main/java/com/pando/app/features/home/ui/chat/ChatFragment.kt
@@ -0,0 +1,16 @@
+package com.pando.app.features.home.ui.chat
+
+import com.pando.app.core.base.BaseFragment
+import com.pando.app.databinding.FragmentChatBinding
+
+class ChatFragment : BaseFragment<FragmentChatBinding>(FragmentChatBinding::inflate) {
+    override fun initData() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun initActionView() {
+    }
+
+}
---
+++ app/src/main/java/com/pando/app/features/home/ui/chatmenu/ChatMenuFragment.kt
@@ -0,0 +1,15 @@
+package com.pando.app.features.home.ui.chatmenu
+
+import com.pando.app.core.base.BaseFragment
+import com.pando.app.databinding.FragmentChatMenuBinding
+
+class ChatMenuFragment : BaseFragment<FragmentChatMenuBinding>(FragmentChatMenuBinding::inflate) {
+    override fun initData() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun initActionView() {
+    }
+}
