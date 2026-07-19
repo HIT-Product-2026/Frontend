688d46bf0738e0378f8d81299f2aa12b579b3395
Toan040606 <toannickson2k60604@gmail.com>
2026-07-19 15:45:29 +0700

    chore: update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,23 +52,23 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
 
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.activity:activity-ktx:1.13.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+    implementation("com.github.bumptech.glide:glide:5.0.9")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("id.zelory:compressor:3.0.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("io.github.chaosleung:pinview:1.4.4")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("androidx.security:security-crypto:1.1.0")
+    implementation("com.google.android.gms:play-services-location:21.4.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("io.coil-kt.coil3:coil:3.3.0")
+    implementation("io.coil-kt.coil3:coil:3.5.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.auth0.android:jwtdecode:2.0.2")
 
-    val lifecycle_version = "2.8.7"
+    val lifecycle_version = "2.11.0"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
@@ -78,9 +78,9 @@ dependencies {
     ksp("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:${room_version}")
 
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.4.0")
 
     val nav_version = "2.9.8"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
@@ -88,15 +88,15 @@ dependencies {
     implementation("androidx.navigation:navigation-dynamic-features-fragment:${nav_version}")
     androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
 
-    val hilt_version = "2.59.2"
+    val hilt_version = "2.60.1"
     implementation("com.google.dagger:hilt-android:$hilt_version")
     ksp("com.google.dagger:hilt-android-compiler:$hilt_version")
 
-    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
-    implementation("com.google.firebase:firebase-messaging")
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.messaging)
 
-    val cameraxVersion = "1.4.0"
+    val cameraxVersion = "1.6.1"
     implementation("androidx.camera:camera-core:$cameraxVersion")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.9.8" apply false
-    id("com.google.dagger.hilt.android") version "2.59.2" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
 }
